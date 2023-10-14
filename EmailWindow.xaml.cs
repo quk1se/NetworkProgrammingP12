cc52fb1ab5065d847bb4db9b1de1cf8dfa32e1f2
alexandr <KalX_si54@student.itstep.org>
2023-10-14 11:01:50 +0300

    mail privacy

--- EmailWindow.xaml.cs
+++ EmailWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,74 +26,124 @@ namespace NetworkProgrammingP12
         {
             InitializeComponent();
         }
-
-        private void SendButton1_Click(object sender, RoutedEventArgs e)
+        private SmtpClient? GetSmtpClient()
         {
-            #region config
-            string host = App.GetConfiguration("smtp:host");
-            if (host == "Error")
-            {
-                MessageBox.Show("JSON File error. Check your file",
-                    "File error", MessageBoxButton.OK, MessageBoxImage.Error);
-                this.Close();
-                return;
-            }
+            #region get and check config
+            String? host = App.GetConfiguration("smtp:host");
             if (host == null)
             {
                 MessageBox.Show("Error getting host");
-                return;
+                return null;
             }
-            string portString = App.GetConfiguration("smtp:port");
+            String? portString = App.GetConfiguration("smtp:port");
             if (portString == null)
             {
                 MessageBox.Show("Error getting port");
-                return;
+                return null;
             }
             int port;
             try { port = int.Parse(portString); }
-            catch { MessageBox.Show("Error parsing port"); return; }
-            string? email = App.GetConfiguration("smtp:email");
+            catch
+            {
+                MessageBox.Show("Error parsing port");
+                return null;
+            }
+            String? email = App.GetConfiguration("smtp:email");
             if (email == null)
             {
                 MessageBox.Show("Error getting email");
-                return;
+                return null;
             }
-            string? password = App.GetConfiguration("smtp:password");
+            String? password = App.GetConfiguration("smtp:password");
             if (password == null)
             {
                 MessageBox.Show("Error getting password");
-                return;
+                return null;
             }
-            string? sslString = App.GetConfiguration("smtp:ssl");
+            String? sslString = App.GetConfiguration("smtp:ssl");
             if (sslString == null)
             {
                 MessageBox.Show("Error getting ssl");
-                return;
+                return null;
             }
             bool ssl;
             try { ssl = bool.Parse(sslString); }
             catch
             {
                 MessageBox.Show("Error parsing ssl");
-                return;
+                return null;
             }
             #endregion
-            if (!textBoxTo.Text.Contains("@"))
+
+            if (!textBoxTo.Text.Contains('@'))
             {
-                MessageBox.Show("Invalid email, try again");
-                return;
+                MessageBox.Show("Введіть правильну адресу");
+                return null;
             }
-            using SmtpClient smtpClient = new(host, port)
+            return new(host, port)
             {
                 EnableSsl = ssl,
                 Credentials = new NetworkCredential(email, password)
             };
+        }
+        private void SendButton1_Click(object sender, RoutedEventArgs e)
+        {
+            SmtpClient smtpClient = GetSmtpClient();
+            if (smtpClient == null) { return; }
             smtpClient.Send(
-                email,
+                App.GetConfiguration("smtp:email")!,
                 textBoxTo.Text,
                 textBoxSubject.Text,
                 textBoxMessage.Text
             );
+            MessageBox.Show("Sended");
+        }
+
+        private void SendButton2_Click(object sender, RoutedEventArgs e)
+        {
+            SmtpClient smtpClient = GetSmtpClient();
+            if (smtpClient == null) { return; }
+            MailMessage mailMessage = new(
+                App.GetConfiguration("smtp:email")!,
+                textBoxTo.Text,
+                textBoxSubject.Text,
+                textBoxHtml.Text)
+            {
+                IsBodyHtml = true,
+            };
+            ContentType pngType = new("image/png");
+
+            ContentType mp3Type = new("audio/mpeg");
+            mailMessage.Attachments.Add(new Attachment("coin25.png", pngType));
+            mailMessage.Attachments.Add(new Attachment("Jump_01.mp3",mp3Type));
+            smtpClient.Send(mailMessage);
+            MessageBox.Show("Sended");
+        }
+
+        private void SendButton3_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            char[] code = new char[6];
+            for (int i = 0; i < 6; i++)
+            {
+                code[i] = chars[random.Next(chars.Length)];
+            }
+            SmtpClient smtpClient = GetSmtpClient();
+            if (smtpClient == null) { return; }
+            MailMessage mailMessage = new(
+                App.GetConfiguration("smtp:email")!,
+                textBoxTo.Text,
+                "Verification code",
+                new string(code))
+            {
+                IsBodyHtml = true,
+            };
+            ContentType privacyTxt = new("text/plain");
+            ContentType privacyDoc = new("application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+            mailMessage.Attachments.Add(new Attachment("privacy.txt", privacyTxt));
+            mailMessage.Attachments.Add(new Attachment("privacy.docx", privacyDoc));
+            smtpClient.Send(mailMessage);
             MessageBox.Show("Sended");
         }
     }
